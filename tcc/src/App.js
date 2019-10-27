cfebeb413f1d028685f24d01531a73db1d920965
Exiou <exiougaming@gmail.com>
2019-10-26 23:02:21 -0300

    é isto rapeize a base ta feita

---
+++ tcc/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+
+export default function src() {
+  return (
+    <div>
+        <header>
+            <h1>Título</h1>
+        </header>
+        <section>
+            <div>
+                Testando...
+            </div>
+            <div>
+
+            </div>
+        </section>
+        <footer>
+            <p>yaay</p>
+        </footer>
+    </div>
+  );
+}
