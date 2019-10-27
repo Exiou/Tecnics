5732f57ded42776f86ff0f8f0a56003cc8f612f3
Exiou <exiougaming@gmail.com>
2019-10-26 23:12:55 -0300

    a

--- tcc/src/App.js
+++ tcc/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css'
 
-export default function src() {
-  return (
-    <div>
-        <header>
-            <h1>Título</h1>
-        </header>
-        <section>
+class App extends Component {
+    render() {
+        return (
             <div>
-                Testando...
-            </div>
-            <div>
+                <header>
+                    <h1>Título</h1>
+                </header>
+                <section>
+                    <div>
+                        Testando...
+                    </div>
+                    <div>
 
+                    </div>
+                </section>
+                <footer>
+                    <p>yaay</p>
+                </footer>
             </div>
-        </section>
-        <footer>
-            <p>yaay</p>
-        </footer>
-    </div>
-  );
+        );
+    }
 }
+
+export default App
