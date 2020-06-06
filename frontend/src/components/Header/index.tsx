a18545bd5e92a905ba9d5be6d2a969d22c731d83
Ryan Alves de Oliveira <ryan.adoliveira23@gmail.com>
2020-06-06 19:08:59 -0300

    header product props, Link, css refactor

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 import './styles.css'
 
 import tecnicsLogo from '../../assets/tecnics-logo.png'
 import hamburgerIcon from '../../assets/svgs/hamburger-menu.svg'
 import avatarIcon from '../../assets/svgs/avatar.svg'
+import downArrow from '../../assets/svgs/down-arrow.svg'
 
-const Header: React.FC = () => {
+interface Props {
+    product?: string
+}
+
+const Header: React.FC<Props> = ({ product }) => {
   
   return (
     <div className="Header">
         <header>
-            <a href="/" className="logo">
+            <Link to="/" className="logo">
                 <img id="tecnicsLogo" src={tecnicsLogo} alt="Tecnics Logo" />
-            </a>
-            <div className="header-right">
-                <a href="/profile" id="avatarButton">
+            </Link>
+            { product ?
+                <h1>{product.charAt(0).toUpperCase() + product.slice(1)}<img src={downArrow} alt="down arrow"/></h1>
+                :
+                ''
+            }
+            <nav>
+                <Link to="/profile" id="avatarButton">
                     <img className="avatar" src={avatarIcon} alt="Avatar Icon"/>
-                </a>
-                <a href="/" id="hamburgerButton">
+                </Link>
+                <Link to="/" id="hamburgerButton">
                     <img className="hamburger" src={hamburgerIcon} alt="Hamburger Icon"/>
-                </a>
-            </div>
+                </Link>
+            </nav>
         </header>
     </div>
   );
