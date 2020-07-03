e85476bcc76d3208b681bd18df7cea507c7d2f01
Ryan Alves de Oliveira <ryan.adoliveira23@gmail.com>
2020-07-03 16:17:55 -0300

    SessionController

---
+++ backend/src/controllers/SessionController.ts
@@ -0,0 +1,40 @@
+import { Request, Response, Router } from 'express'
+
+import User from '../models/User'
+
+class SessionController {
+
+  public path = '/sessions'
+  public routes = Router()
+
+  constructor() {
+    this.initializeRoutes()
+  }
+
+  public initializeRoutes() {
+    this.routes.post(`${this.path}`, this.store)
+  }
+
+  public async store (req: Request, res: Response): Promise<Response> {
+    try {
+
+      const { name, password } = req.body
+
+      const user = await User.findOne({
+        nome: name,
+        senha: password
+      })
+
+      if(!user){
+        return res.status(400).json({ error: 'No user found with these credentials' })
+      }
+
+      return res.json(user)
+      
+    } catch (err) {
+      return res.send(`Ocorreu um erro na requisição: ${err}`)
+    }
+  }
+}
+
+export default new SessionController()
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import UserController from './controllers/UserController'
+import SessionController from './controllers/SessionController'
 import ProdutoController from './controllers/ProdutoController'
 import LojaController from './controllers/LojaController'
 import Loja from './models/Loja'
@@ -7,6 +8,7 @@ Loja
 
 const routes = [
     UserController.routes,
+    SessionController.routes,
     ProdutoController.routes,
     LojaController.routes
 ]
