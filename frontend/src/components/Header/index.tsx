5aaea5fcc4f8ae83672cd7c885e5b604344b9300
Ryan Alves de Oliveira <ryan.adoliveira23@gmail.com>
2020-07-04 10:27:40 -0300

    exit button

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 import './styles.css'
 
 import tecnicsLogo from '../../assets/tecnics-logo.png'
-import hamburgerIcon from '../../assets/svgs/hamburger-menu.svg'
 import avatarIcon from '../../assets/svgs/avatar.svg'
 import downArrow from '../../assets/svgs/down-arrow.svg'
 
@@ -15,41 +14,41 @@ interface Props {
 
 const Header: React.FC<Props> = ({ product }) => {
 
-    const userId = localStorage.getItem('userId')
+    const userId = localStorage.getItem('userId')    
   
-  return (
-    <div className="Header">
-        <header>
-            <Link to="/" className="logo">
-                <img id="tecnicsLogo" src={tecnicsLogo} alt="Tecnics Logo" />
-            </Link>
-            { product ? <h1>{product.charAt(0).toUpperCase() + product.slice(1)}<img src={downArrow} alt="down arrow"/></h1>
-                : ''
-            }
-            {
-                userId ? (
-                    <nav>
-                        <Link to="/profile" id="avatarButton">
-                            <img className="avatar" src={avatarIcon} alt="Avatar Icon"/>
-                        </Link>
-                        <Link to="/" id="hamburgerButton">
-                            <img className="hamburger" src={hamburgerIcon} alt="Hamburger Icon"/>
-                        </Link>
-                    </nav>
-                ) : (
-                    <nav>
-                        <Link to="/users/login" className="text">
-                            <span>Entrar</span>
-                        </Link>
-                        <Link to="/users/register" className="text">
-                            <span>Cadastrar</span>
-                        </Link>
-                    </nav>
-                )
-            }
-        </header>
-    </div>
-  );
+    return (
+        <div className="Header">
+            <header>
+                <Link to="/" className="logo">
+                    <img id="tecnicsLogo" src={tecnicsLogo} alt="Tecnics Logo" />
+                </Link>
+                { product ? <h1>{product.charAt(0).toUpperCase() + product.slice(1)}<img src={downArrow} alt="down arrow"/></h1>
+                    : ''
+                }
+                {
+                    userId ? (
+                        <nav>
+                            <Link to="/users/profile" id="avatarButton">
+                                <img className="avatar" src={avatarIcon} alt="Avatar Icon"/>
+                            </Link>
+                            <Link to="/" className="text" onClick={() => (localStorage.removeItem('userId'))}>
+                                <span>Sair</span>
+                            </Link>
+                        </nav>
+                    ) : (
+                        <nav>
+                            <Link to="/users/login" className="text">
+                                <span>Entrar</span>
+                            </Link>
+                            <Link to="/users/register" className="text">
+                                <span>Cadastrar</span>
+                            </Link>
+                        </nav>
+                    )
+                }
+            </header>
+        </div>
+    );
 }
 
 export default Header;
