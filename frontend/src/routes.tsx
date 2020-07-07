413ce6cfda7bb2ba8ad6c71c7f8fc4fcd6ced556
Ryan Alves de Oliveira <ryan.adoliveira23@gmail.com>
2020-07-06 21:27:46 -0300

    Detail page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Main from './pages/Main'
 import Product from './pages/Product'
+import Detail from './pages/Detail'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
@@ -15,6 +16,7 @@ export default function Routes(){
         <Switch>
             <Route path="/" exact component={Main}/>
             <Route path="/produtos/:product" exact component={Product}/>
+            <Route path="/produtos/:product/:productId" exact component={Detail}/>
             <Route path="/users/register" exact component={Register}/>
             <Route path="/users/login" exact component={Login}/>
             <Route path="/users/profile" exact component={Profile}/>
