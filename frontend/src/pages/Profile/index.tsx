c0194b5bad868798cd3074d12fe656463220f79a
Ryan Alves de Oliveira <ryan.adoliveira23@gmail.com>
2020-07-30 20:20:03 -0300

    profile page favorites functionality and styling

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -11,6 +11,28 @@ import userIcon from '../../assets/svgs/user.svg'
 import mailIcon from '../../assets/svgs/mail.svg'
 import passwordIcon from '../../assets/svgs/password.svg'
 import editProfileIcon from '../../assets/svgs/edit_profile.svg'
+import plusIcon from '../../assets/svgs/plus.svg'
+import filledHeartIcon from '../../assets/svgs/filled_heart.svg'
+
+interface Favoritos {
+  _id: string
+  produto: Products
+  modelo: string
+}
+interface Products {
+  _id: string
+  nome: string
+  fabricante: string
+  modelo: string
+  imagem: string
+  imagem_url: string
+  lojas: {
+    id: string
+    idLoja: string
+    preco: number
+    urlProduto: string
+  }[]
+}
 
 function Profile () {
   const userId = window.localStorage.getItem('userId')
@@ -25,11 +47,13 @@ function Profile () {
   })
   const [toggleShowPassword, setToggleShowPassword] = useState('password')
   const [selectedFile, setSelectedFile] = useState<File>()
+  const [favoritos, setFavoritos] = useState<Favoritos[]>([])
 
   useEffect(() => {
-    api.get(`/users/${userId}`).then(response => (
+    api.get(`/users/${userId}`).then(response => {
       setFormData(response.data)
-    ))
+      setFavoritos(response.data.favoritos)
+    })
   }, [userId])
 
   function handleInput (event: FocusEvent<HTMLInputElement>) {
@@ -63,23 +87,97 @@ function Profile () {
     setSelectedFile(image)
   }
 
+  async function handleFavorites (id: string) {
+    await api.delete(`/favoritos/${id}`, {
+      headers: {
+        userid: userId
+      }
+    })
+    setFavoritos(favoritos.filter(product => product.produto._id !== id))
+  }
+
+  const formatter = new Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
   return (
     <div className="Profile">
       <div id="main">
 
         <div id="left-side">
-          <img src={tecnicsLogo} alt="Tecnics Logo"/>
+          <img id="tecnics-logo" src={tecnicsLogo} alt="Tecnics Logo"/>
 
+          <h1>Favoritos</h1>
           <div id="favorites">
-            <h1>Favoritos</h1>
+            <div>
+              {
+                favoritos.length > 0
+                  ? favoritos.map(productData => {
+                    let modelo = ''
+                    switch (productData.modelo) {
+                      case 'Processador':
+                        modelo = 'processadores'
+                        break
+                      case 'PlacaVideo':
+                        modelo = 'placas-video'
+                        break
+                      case 'PlacaMae':
+                        modelo = 'placas-mae'
+                        break
+                      case 'Memoria':
+                        modelo = 'memorias'
+                        break
+                      case 'Gabinete':
+                        modelo = 'gabinetes'
+                        break
+                      case 'Fonte':
+                        modelo = 'fontes'
+                        break
+                      case 'Cooler':
+                        modelo = 'coolers'
+                        break
+                      case 'Armazenamento':
+                        modelo = 'armazenamentos'
+                        break
+                      default:
+                        break
+                    }
+                    return (
+                      <div className='card' key={productData.produto.modelo}>
+                        <img className="product-image" src={productData.produto.imagem_url} alt={productData.produto.imagem}/>
 
-            <p>
-              Dê um "coração"
-              <br/>
-              nos seus produtos favoritos e
-              <br/>
-              eles aparecerão aqui!
-            </p>
+                        <div className="props">
+                          <h2>{productData.produto.fabricante}</h2>
+                          <h2>{productData.produto.nome}</h2>
+                          <h3>{formatter.format(productData.produto.lojas[0].preco)}</h3>
+                        </div>
+
+                        <Link to={`../produtos/${modelo}/${productData.produto._id}`} >
+                          <label>Detalhes</label>
+                          <img className="plus-icon" src={plusIcon} alt="Plus Icon"/>
+                        </Link>
+
+                        <button onClick={() => handleFavorites(productData.produto._id)}>
+                          <img
+                            className="heart-icon"
+                            src={filledHeartIcon}
+                            alt="Heart Icon"
+                          />
+                        </button>
+
+                      </div>
+                    )
+                  })
+                  : <p>
+                  Dê um "coração"
+                    <br/>
+                  nos seus produtos favoritos e
+                    <br/>
+                  eles aparecerão aqui!
+                  </p>
+              }
+            </div>
           </div>
         </div>
         <section>
